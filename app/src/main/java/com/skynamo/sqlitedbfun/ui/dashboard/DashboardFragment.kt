aa18f1cd0eaf5dadce441f1791f2fcdbd27a855b
Diederik <djh@skynamo.com>
2024-05-02 21:24:52 +0200

    Add flutter view on second page

--- app/src/main/java/com/skynamo/sqlitedbfun/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/skynamo/sqlitedbfun/ui/dashboard/DashboardFragment.kt
@@ -6,12 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
+import com.skynamo.sqlitedbfun.R
 import com.skynamo.sqlitedbfun.databinding.FragmentDashboardBinding
+import io.flutter.embedding.android.FlutterFragment
 
 class DashboardFragment : Fragment() {
-
-private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -34,7 +36,35 @@ private var _binding: FragmentDashboardBinding? = null
     return root
   }
 
-override fun onDestroyView() {
+    companion object {
+        private const val TAG_FLUTTER_FRAGMENT = "flutter_fragment"
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val fragmentManager: FragmentManager = childFragmentManager
+
+        // Attempt to find an existing FlutterFragment, in case this is not the
+        // first time that onCreate() was run.
+        var flutterFragment = fragmentManager
+            .findFragmentByTag(TAG_FLUTTER_FRAGMENT) as FlutterFragment?
+
+        // Create and attach a FlutterFragment if one does not exist.
+        if (flutterFragment == null) {
+            flutterFragment = FlutterFragment.createDefault()
+            fragmentManager
+                .beginTransaction()
+                .add(
+                    R.id.flutter_host,
+                    flutterFragment,
+                    TAG_FLUTTER_FRAGMENT
+                )
+                .commit()
+        }
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
